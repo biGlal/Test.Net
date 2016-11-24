3d19e43d3a8c4408dfb8e0f490809b2c63337a33
biGlal <bilal.sabbagh@gmail.com>
2016-11-24 20:35:46 +0400

    dotnet start

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test.Net
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Test.Net World!");
+            Console.Read();
+        }
+    }
+}
